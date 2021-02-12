07c40f96d8b6c5cf242b5e904cb3355e56d7fa94
Christian Felsing <c@felsing.net>
2021-02-12 17:46:16 +0100

    Test for random defined

--- src/app/crypt/crypt.component.spec.ts
+++ src/app/crypt/crypt.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CryptComponent } from './crypt.component';
+import {interval} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 describe('CryptComponent', () => {
   let component: CryptComponent;
@@ -19,8 +21,22 @@ describe('CryptComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  const waitUntil = async (untilTruthy: () => any): Promise<boolean> => {
+    while (!untilTruthy()) {
+      await interval(25).pipe(take(1)).toPromise();
+    }
+    return Promise.resolve(true);
+  };
+
+  it('should create', async (done) => {
     expect(component).toBeTruthy();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const b = await waitUntil(() => {
+      console.log('random: %o', component.random);
+      done();
+    });
+    expect(b).toBeDefined();
   });
 
 });
